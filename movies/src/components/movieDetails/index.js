2044e57522fc450b43c68fbefdfab2b234481fdb
TheMarkRyan <20098241@mail.wit.ie>
2023-11-21 15:14:17 +0000

    Full review page.

--- movies/src/components/movieDetails/index.js
+++ movies/src/components/movieDetails/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import React, { useState } from "react";
 import Chip from "@mui/material/Chip";
 import Paper from "@mui/material/Paper";
--- movies/src/components/movieReviews/index.js
+++ movies/src/components/movieReviews/index.js
@@ -8,7 +8,7 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Link } from "react-router-dom";
 import { getMovieReviews } from "../../api/tmdb-api";
-import { excerpt } from "../../util";
+import { excerpt } from "../../utils";
 
 export default function MovieReviews({ movie }) {
   const [reviews, setReviews] = useState([]);
